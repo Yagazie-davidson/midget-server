80ed5be2d9995acca4d125f0994b62aa2459bbfa
letam <davidsonletam@gmail.com>
2023-07-17 15:12:45 +0100

    change the base route for urls

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 connectDB();
 
 // app.use("/", homeRoutes);
-app.use("/api", urlRoutes);
+app.use("/", urlRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
